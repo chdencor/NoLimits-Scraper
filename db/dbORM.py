b641575513dc9f5d52b104f53cfb217ed3d20e0f
chdencor <lucasignaciodencor@gmail.com>
2024-10-12 12:43:06 -0300

    implemented PL/PgSQL functions

--- db/dbORM.py
+++ db/dbORM.py
@@ -8,75 +8,87 @@ class dbORM:
         self.session = self.Session()
 
     def close(self):
+        """Cerrar la sesión."""
         self.session.close()
 
-    def batch_insert(self, table_name, values_list):
-        """Realizar inserciones por lotes en la tabla especificada."""
+    def insert_relationship(self, insert_function, **kwargs):
+        """Método genérico para insertar relaciones en la base de datos."""
         try:
-            if values_list:
-                # Crear una consulta con múltiples valores
-                columns = values_list[0].keys()
-                placeholders = ', '.join(f':{col}' for col in columns)
-                insert_query = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({placeholders})"
-                
-                # Ejecutar el batch insert
-                self.session.execute(text(insert_query), values_list)
-                self.session.commit()
-        except Exception as e:
-            self.session.rollback()
-            print(f"Error al insertar en {table_name}: {e}")
-
-    def insert_api_batch(self, api_list):
-        """Insertar múltiples registros en la tabla 'api'."""
-        self.batch_insert('api', api_list)
-
-    def insert_register_batch(self, register_list):
-        """Insertar múltiples registros en la tabla 'registros'."""
-        self.batch_insert('registros', register_list)
-
-    def insert_criptomoneda_batch(self, cripto_list):
-        """Insertar múltiples registros en la tabla 'criptomonedas'."""
-        self.batch_insert('criptomonedas', cripto_list)
-
-    def insert_api_relationship_batch(self, table_name, relationship_list, unique_columns):
-        """Insertar múltiples relaciones en la tabla especificada, evitando duplicados."""
-        try:
-            for relationship in relationship_list:
-                filters = ' AND '.join([f"{col} = :{col}" for col in unique_columns])
-                query = f"SELECT 1 FROM {table_name} WHERE {filters}"
-    
-                # Verificar si la relación ya existe
-                existing_relationship = self.session.execute(
-                    text(query), {col: relationship[col] for col in unique_columns}
-                ).fetchone()
-    
-                # Si no existe, insertar la nueva relación
-                if not existing_relationship:
-                    self.batch_insert(table_name, [relationship])
-                else:
-                    print(f"La relación en {table_name} ya existe: {relationship}")
-    
+            self.session.execute(text(f"SELECT {insert_function}({', '.join(f':{k}' for k in kwargs.keys())});"), kwargs)
             self.session.commit()
         except Exception as e:
             self.session.rollback()
-            print(f"Error al insertar relaciones en {table_name}: {e}")
+            print(f"Error al insertar en {insert_function}: {e}")
 
-    # Métodos para cada tabla intermedia
-    def insert_api_criptomoneda_batch(self, relationship_list):
-        self.insert_api_relationship_batch('api_criptomonedas', relationship_list, ['api_id', 'criptomoneda_id'])
+    def insert_api(self, nombre, url, metodo, cantidad_parametros, tipo_autenticacion, 
+                   formato_respuesta, limite_uso, tiempo_respuesta_promedio_ms, estado='activo'):
+        """Insertar un nuevo registro en la tabla 'api'."""
+        self.insert_relationship('insert_api', nombre=nombre, url=url, metodo=metodo, 
+                                 cantidad_parametros=cantidad_parametros, 
+                                 tipo_autenticacion=tipo_autenticacion, 
+                                 formato_respuesta=formato_respuesta, 
+                                 limite_uso=limite_uso, 
+                                 tiempo_respuesta_promedio_ms=tiempo_respuesta_promedio_ms, 
+                                 estado=estado)
 
-    def insert_api_mercado_batch(self, relationship_list):
-        self.insert_api_relationship_batch('api_mercados', relationship_list, ['api_id', 'mercado_id'])
+    def insert_register(self, criptomoneda_id, price_usd, percent_change_24h, percent_change_1h,
+                        percent_change_7d, price_btc, market_cap_usd, volume24, volume24a, csupply, tsupply):
+        """Insertar un nuevo registro en la tabla 'registros' usando la función de PostgreSQL."""
+        self.insert_relationship('insert_register', criptomoneda_id=criptomoneda_id, 
+                                 price_usd=price_usd, percent_change_24h=percent_change_24h, 
+                                 percent_change_1h=percent_change_1h, 
+                                 percent_change_7d=percent_change_7d, price_btc=price_btc, 
+                                 market_cap_usd=market_cap_usd, volume24=volume24, 
+                                 volume24a=volume24a, csupply=csupply, tsupply=tsupply)
 
-    def insert_api_registro_batch(self, relationship_list):
-        self.insert_api_relationship_batch('api_registros', relationship_list, ['api_id', 'registro_id'])
+    def insert_multiple_api_criptomoneda(self, api_ids, criptomoneda_ids):
+        """Insertar múltiples relaciones entre API y criptomonedas en la tabla 'api_criptomonedas'."""
+        print(f"Inserting multiple API - Criptomonedas")
+        self.insert_relationship('insert_multiple_api_criptomonedas', api_ids=api_ids, criptomoneda_ids=criptomoneda_ids)
 
-    def insert_api_metrica_general_batch(self, relationship_list):
-        self.insert_api_relationship_batch('api_metricas_generales', relationship_list, ['api_id', 'metrica_general_id'])
+    def insert_multiple_api_mercado(self, api_ids, mercado_ids):
+        """Insertar múltiples relaciones entre API y mercados en la tabla 'api_mercados'."""
+        self.insert_relationship('insert_multiple_api_mercados', api_ids=api_ids, mercado_ids=mercado_ids)
 
-    def insert_api_exchange_batch(self, relationship_list):
-        self.insert_api_relationship_batch('api_exchanges', relationship_list, ['api_id', 'exchange_id'])
+    def insert_multiple_api_registro(self, api_ids, registro_ids):
+        """Insertar múltiples relaciones entre API y registros en la tabla 'api_registros'."""
+        self.insert_relationship('insert_multiple_api_registros', api_ids=api_ids, registro_ids=registro_ids)
 
+    def insert_multiple_api_metrica_general(self, api_ids, metrica_ids):
+        """Insertar múltiples relaciones entre API y métricas generales en la tabla 'api_metricas_generales'."""
+        self.insert_relationship('insert_multiple_api_metricas_generales', api_ids=api_ids, metrica_ids=metrica_ids)
+
+    def insert_multiple_api_exchange(self, api_ids, exchange_ids):
+        """Insertar múltiples relaciones entre API y exchanges en la tabla 'api_exchanges'."""
+        self.insert_relationship('insert_multiple_api_exchanges', api_ids=api_ids, exchange_ids=exchange_ids)
+
+    def insert_exchange(self, name, name_id, volume_usd, active_pairs, country, url):
+        """Insertar un nuevo registro en la tabla 'exchanges'."""
+        self.insert_relationship('insert_exchange', name=name, name_id=name_id, 
+                                 volume_usd=volume_usd, active_pairs=active_pairs, 
+                                 country=country, url=url)
+
+    def insert_mercado_activo(self, market_name, base_symbol, quote_symbol, price_usd, volume_usd, exchange_id):
+        """Insertar un nuevo registro en la tabla 'mercados_activos'."""
+        self.insert_relationship('insert_mercado_activo', market_name=market_name, 
+                                 base_symbol=base_symbol, quote_symbol=quote_symbol, 
+                                 price_usd=price_usd, volume_usd=volume_usd, 
+                                 exchange_id=exchange_id)
+
+    def insert_metrica_general(self, coins_count, active_markets, total_mcap, total_volume, 
+                                btc_d, eth_d, mcap_change, volume_change, 
+                                avg_change_percent, volume_ath, mcap_ath):
+        """Insertar un nuevo registro en la tabla 'metrica_general'."""
+        self.insert_relationship('insert_metrica_general', coins_count=coins_count, 
+                                 active_markets=active_markets, total_mcap=total_mcap, 
+                                 total_volume=total_volume, btc_d=btc_d, eth_d=eth_d, 
+                                 mcap_change=mcap_change, volume_change=volume_change, 
+                                 avg_change_percent=avg_change_percent, 
+                                 volume_ath=volume_ath, mcap_ath=mcap_ath)
+
+    def insert_criptomoneda(self, name, symbol, msupply):
+        """Insertar un nuevo registro en la tabla 'criptomonedas'."""
+        self.insert_relationship('insert_criptomoneda', name=name, symbol=symbol, msupply=msupply)
 
     def fetchOneCriptoBySymbol(self, symbol):
         """Obtener una criptomoneda por símbolo."""
@@ -84,7 +96,7 @@ class dbORM:
             SELECT * FROM get_criptomoneda_by_symbol(:symbol);
         """), {'symbol': symbol}).fetchone()
 
-        return result if result else None
+        return result if result else None  # Retornar el resultado o None si no existe
     
     def fetchOneApiByName(self, nombre):
         """Obtener una API por nombre."""
@@ -96,4 +108,4 @@ class dbORM:
     def fetchOneRegisterId(self, criptoId):
         query = text("SELECT id FROM registros WHERE criptomoneda_id = :criptoId")
         result = self.session.execute(query, {'criptoId': criptoId}).fetchone()
-        return result.id if result else None
+        return result.id if result else None  # Retorna solo el ID, o None si no hay resultado
--- utils/dbBroker.py
+++ utils/dbBroker.py
@@ -4,107 +4,79 @@ from services.RawRegisterService import RegistroService
 from db.dbORM import dbORM
 from models.APICriptoTicker import APICripto
 
-def dataLoader(dbInstance):
-    cripto = CriptoService()
-    registroService = RegistroService()
-
-    # Crea una instancia de la API
-    api = APICripto()
-
-    # Insertar la información de la API antes de cualquier otra inserción
+def insert_api_if_not_exists(dbInstance, api_name, api_url):
+    """Inserta la API en la base de datos si no existe."""
     try:
-        api_name = "CoinLore"
-        api_url = api.url
-
-        # Verificar si la API ya existe y evitar duplicados
         existing_api = dbInstance.fetchOneApiByName(api_name)
         if not existing_api:
-            dbInstance.insert_api_batch([{'name': api_name, 'url': api_url, 'method': 'GET', 'auth_required': 0, 
-                                           'rate_limit': None, 'response_format': 'JSON', 'headers': None, 'params': None}])
+            dbInstance.insert_api(api_name, api_url, 'GET', 0, None, 'JSON', None, None)
             print(f"API insertada: {api_name} en {api_url}")
-
-        # Obtener el ID de la API recién insertada
-        api_id = dbInstance.fetchOneApiByName(api_name).id
-
+            return dbInstance.fetchOneApiByName(api_name).id 
+        return existing_api.id
     except Exception as e:
         print(f"Error al insertar la API: {str(e)}")
-        return  # Salir de la función si hay un error en la API
+        return None
 
-    while True:
-        data = cripto.parsed_data
-        idsObtenidos = []
-        criptomonedas_batch = []
-        registros_batch = []
-        api_criptomonedas_batch = []
-        api_registros_batch = []
+def process_new_cryptos(dbInstance, cripto, api_id):
+    """Procesa nuevas criptomonedas y registra su información."""
+    registroService = RegistroService()
+    ids_obtenidos = []
 
-        # Obtener y procesar nuevas criptomonedas
-        idsNuevos = registroService.criptoNueva()
-        for new_id in idsNuevos:
-            if new_id not in idsObtenidos:
-                item = cripto.getCriptoData(new_id)
-                if item:
-                    data.append(item)
+    # Obtener y procesar nuevas criptomonedas
+    ids_nuevos = registroService.criptoNueva()
+    for new_id in ids_nuevos:
+        if new_id not in ids_obtenidos:
+            item = cripto.getCriptoData(new_id)
+            if item:
+                ids_obtenidos.append(item['id'])  # Guardar el ID para evitar duplicados
+                existingCripto = dbInstance.fetchOneCriptoBySymbol(item['symbol'])
 
-        for item in data:
-            idsObtenidos.append(item['id'])
-
-            existingCripto = dbInstance.fetchOneCriptoBySymbol(item['symbol'])
-            try:
                 if not existingCripto:
-                    # Preparar los datos para la inserción por lotes de criptomonedas
+                    # Insertar la nueva criptomoneda
                     msupply = item.get('msupply') or 0
-                    criptomonedas_batch.append({
+                    dbInstance.insert_criptomoneda({
                         'name': item['name'],
                         'symbol': item['symbol'],
                         'msupply': msupply
                     })
+                    # Obtener el ID de la criptomoneda recién insertada
+                    existingCripto = dbInstance.fetchOneCriptoBySymbol(item['symbol'])
 
-            except Exception as e:
-                print(f"Error al crear la criptomoneda: {item['name']} - {str(e)}")
-                continue
+                # Extraer valores para el registro
+                registro_values = (
+                    existingCripto.id if existingCripto else None,
+                    float(item.get('price_usd') or 0),
+                    float(item.get('percent_change_24h') or 0),
+                    float(item.get('percent_change_1h') or 0),
+                    float(item.get('percent_change_7d') or 0),
+                    float(item.get('price_btc') or 0),
+                    float(item.get('market_cap_usd') or 0),
+                    float(item.get('volume24') or 0),
+                    float(item.get('volume24a') or 0),
+                    float(item.get('csupply') or 0),
+                    float(item.get('tsupply') or 0)
+                )
 
-            registroValues = {
-                'criptomoneda_id': existingCripto.id if existingCripto else None,
-                'price_usd': float(item.get('price_usd') or 0),
-                'percent_change_24h': float(item.get('percent_change_24h') or 0),
-                'percent_change_1h': float(item.get('percent_change_1h') or 0),
-                'percent_change_7d': float(item.get('percent_change_7d') or 0),
-                'price_btc': float(item.get('price_btc') or 0),
-                'market_cap_usd': float(item.get('market_cap_usd') or 0),
-                'volume24': float(item.get('volume24') or 0),
-                'volume24a': float(item.get('volume24a') or 0),
-                'csupply': float(item.get('csupply') or 0),
-                'tsupply': float(item.get('tsupply') or 0)
-            }
+                
+                dbInstance.insert_register(*registro_values)
 
-            registros_batch.append(registroValues)
+                
+                dbInstance.insert_multiple_api_criptomoneda([{
+                    'api_id': api_id,
+                    'criptomoneda_id': existingCripto.id if existingCripto else None
+                }])
 
-            if existingCripto:
-                api_criptomonedas_batch.append({'api_id': api_id, 'criptomoneda_id': existingCripto.id})
+def dataLoader(dbInstance):
+    """Carga los datos de las criptomonedas."""
+    cripto = CriptoService()
+    api = APICripto()
 
-        try:
-            # Hacer inserciones por lotes
-            if criptomonedas_batch:
-                dbInstance.insert_criptomoneda_batch(criptomonedas_batch)
-                print(f"Criptomonedas insertadas: {len(criptomonedas_batch)}")
+    
+    api_id = insert_api_if_not_exists(dbInstance, "CoinLore", api.url)
+    if api_id is None:
+        return
 
-            if registros_batch:
-                dbInstance.insert_register_batch(registros_batch)
-                print(f"Registros insertados: {len(registros_batch)}")
-
-            if api_criptomonedas_batch:
-                dbInstance.insert_api_criptomoneda_batch(api_criptomonedas_batch)
-                print(f"Relaciones API-Criptomoneda insertadas: {len(api_criptomonedas_batch)}")
-
-        except Exception as e:
-            print(f"Error durante la inserción por lotes: {e}")
-
-        # Actualizar la información del cripto
-        try:
-            registroService.actualizarCripto(idsObtenidos)
-        except Exception as e:
-            print(f"Error en la actualización de criptomonedas: {e}")
-
-        # Esperar antes de la siguiente iteración
-        time.sleep(61)
+    while True:
+        process_new_cryptos(dbInstance, cripto, api_id)
+        print("Carga de datos completada. Esperando a la próxima ejecución...")
+        time.sleep(40)
